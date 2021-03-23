aa350947368ef9a0b805620a1c3a15f43b5b4ff1
Salvi03 <crissalvi57@gmail.com>
2021-03-23 12:11:50 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <image_component height="100" width="100"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import image_component from './components/image.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    image_component
   }
 }
 </script>
---
+++ src/components/image.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="container-sm">
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">larghezza</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" v-model="width">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">altezza</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" v-model="height">
+        </div>
+      </form>
+      <div class="btn btn-danger btn-lg" @click="cl" type="button">
+        full HD (1920x1080)
+      </div>
+    </div>
+
+    <img src="../assets/image.jpg" alt="taiga" :width="width" :height="height">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'image_component',
+  props: {
+    height: {
+      type: String,
+      required: false
+    },
+
+    width: {
+      type: String,
+      required: false
+    }
+  },
+
+  methods: {
+    cl: function() {
+      this.height = "1080";
+      this.width = "1920";
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
